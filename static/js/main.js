2df9641694b506c81caeb6a9a8e5a662f14f7fe0
JSM-02 <hellojeon02@gmail.com>
2026-06-23 15:36:49 +0900

    feat:역/물품 검색 필터 추가

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,28 @@
 import { initModal, howToUseModal } from './modal.js';
 import { initMap, moveToStation } from './map.js'; 
-import { initSearch, handleSearch } from './search.js';
+import { initSearch } from './search.js';
+import { fetchLostItems } from './api.js';
+import { renderLostList } from './ui.js';
+import { setupPagination } from './pagination.js';
+
+async function handleSearch(keyword, type, page = 1) {
+    if (type === 'location') {
+        moveToStation(keyword); 
+    }
+    try {
+        const data = await fetchLostItems(keyword, type, page);
+        renderLostList(data.items);
+        setupPagination(data.totalItems, (newPage) => {
+            handleSearch(keyword, type, newPage);
+        }, page === 1);
+    } catch (e) {
+        console.error(e);
+        alert('데이터를 불러오는데 실패했습니다.');
+    }
+}
 
 function mainInit() { 
-    initSearch(moveToStation);
+    initSearch(handleSearch);
     howToUseModal();
     initModal();
     initMap((stationName) => {
--- static/js/search.js
+++ static/js/search.js
@@ -1,36 +1,17 @@
-import { fetchLostItems } from './api.js';
-import { renderLostList } from './ui.js';
-import { setupPagination } from './pagination.js';
-
-let moveToStation;
-
-export async function handleSearch(keyword, type, page = 1) {
-    if (type === 'item') {
-        moveToStation(keyword); 
-    }
-    try {
-        const data = await fetchLostItems(keyword, type, page);
-        renderLostList(data.items);
-        setupPagination(data.totalItems, (newPage) => {
-            handleSearch(keyword, type, newPage);
-        }, page === 1);
-    } catch (e) {
-        console.error(e);
-        alert('데이터를 불러오는데 실패했습니다.');
-    }
-}
+let handleSearchFn;
 
 export function onSearch() {
     const keyword = document.getElementById('search').value.trim();
+    const type = document.getElementById("search-filter").value;
     if (!keyword) {
         alert('검색어를 입력하세요.');
         return;
     }
-    handleSearch(keyword, 'item');
+    handleSearchFn(keyword, type);
 }
 
-export function initSearch(moveToStationCallback) {
-    moveToStation = moveToStationCallback;
+export function initSearch(handleSearchCallback) {
+    handleSearchFn = handleSearchCallback;
     document.getElementById('search-btn').addEventListener('click', onSearch);
     document.getElementById('search').addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
